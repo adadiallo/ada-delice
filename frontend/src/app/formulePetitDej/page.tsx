e89d4f765d9199bb0b51fa4f6fe00ef75dbab671
@ada_diallo <adadialo2203@gmail.com>
2025-10-23 17:07:28 +0000

    ajout import footer

--- frontend/src/app/formulePetitDej/page.tsx
+++ frontend/src/app/formulePetitDej/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Navbar from "../Navbar/page";
-import Footer from "../footer/page";
+import Footer from "@/components/footer";
 
 export default function PetitDejeunerPage() {
   const menus = [
