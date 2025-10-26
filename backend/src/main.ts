91acb6186986246902fea5b8e30f98f0eb0ab3c8
@ada_diallo <adadialo2203@gmail.com>
2025-10-26 17:42:38 +0000

    modif main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
 app.enableCors({
-    origin: 'http://localhost:3001', 
+  origin: [
+    'http://localhost:3001',
+    'https://ada-delice-7l4u.vercel.app'
+  ],
+    
     credentials: true, 
   });
   await app.listen(process.env.PORT ?? 3000);
